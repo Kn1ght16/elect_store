812271393946085afc4b157e94f7321819a7d173
Kn1ght16 <Danchenko0@mail.ru>
2023-02-24 16:56:26 +0300

    task1 13.2 items.csv

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-from utils.goods import Goods
+import os
+from utils.goods import Item
+
+path_csv = os.sep.join(["data", "items.csv"])
 
 
 def main():
-    item1 = Goods("Смартфон", 10000, 20)
-    item2 = Goods("Ноутбук", 20000, 5)
+    item1 = Item("Смартфон", 10000, 20)
     print(item1.calculate_price())
-    print(item2.calculate_price())
-    Goods.rate = 0.8
 
+    item2 = Item("Ноутбук", 20000, 5)
+    print(item2.calculate_price())
+
+    Item.rate = 0.8
     item1.discount_price()
+    item2.instantiate_from_csv(path_csv)
 
     print(item1.price)
     print(item2.price)
-    print(item1, item2)
+    try:
+        for item in Item.all:
+            print(item)
+    except NameError:
+        print("Длина названия товара не должна превышать 10 символов.")
+
+    print(Item.is_integer(5))
+    print(Item.is_integer(5.0))
+    print(Item.is_integer(5.5))
 
 
 if __name__ == "__main__":
